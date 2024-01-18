08c69828ba3643942ce902c850cbfdb5cc9c7d56
Piotr Wieczorek <p@wie.cz>
2024-01-18 12:30:14 +0100

    Make voronoi_vizualizer compile when Qt5 is used (may be working under Qt6 as well)

--- voronoi_visualizer.cpp
+++ voronoi_visualizer.cpp
@@ -7,11 +7,22 @@
 
 // See http://www.boost.org for updates, documentation, and revision history.
 
+#include <array>
 #include <iostream>
 #include <vector>
 
-#include <QtOpenGL/QGLWidget>
-#include <QtGui/QtGui>
+#include <QApplication>
+#include <QCheckBox>
+#include <QFileDialog>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QListWidget>
+#include <QMainWindow>
+#include <QMessageBox>
+#include <QOpenGLFunctions>
+#include <QOpenGLWidget>
+#include <QPushButton>
+#include <QTextStream>
 
 #include <boost/polygon/polygon.hpp>
 #include <boost/polygon/voronoi.hpp>
@@ -19,14 +30,35 @@ using namespace boost::polygon;
 
 #include "voronoi_visual_utils.hpp"
 
-class GLWidget : public QGLWidget {
+
+static const char* vertex_shader_code = R"(#ifdef GL_ES
+precision mediump float;
+#endif
+attribute vec2 position;
+uniform mat4 mvpMatrix;
+void main(void) {
+    gl_Position = mvpMatrix * vec4(position, 0.0, 1.0);
+}
+)";
+
+static const char* fragment_shader_code = R"(#ifdef GL_ES
+precision mediump float;
+#endif
+uniform vec4 color;
+void main(void) {
+  gl_FragColor = color;
+}
+)";
+
+class GLWidget : public QOpenGLWidget, public QOpenGLFunctions {
   Q_OBJECT
 
  public:
-  explicit GLWidget(QMainWindow* parent = NULL) :
-      QGLWidget(QGLFormat(QGL::SampleBuffers), parent),
+  explicit GLWidget(QWidget* parent = NULL) :
+      QOpenGLWidget(parent),
       primary_edges_only_(false),
       internal_edges_only_(false) {
+    setUpdateBehavior(QOpenGLWidget::UpdateBehavior::NoPartialUpdate);
     startTimer(40);
   }
 
@@ -77,16 +109,51 @@ class GLWidget : public QGLWidget {
 
  protected:
   void initializeGL() {
+    initializeOpenGLFunctions();
     glHint(GL_POINT_SMOOTH_HINT, GL_NICEST);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_BLEND);
     glEnable(GL_POINT_SMOOTH);
+    vertex_shader_ = prepare_shader(GL_VERTEX_SHADER, vertex_shader_code);
+    fragment_shader_ = prepare_shader(GL_FRAGMENT_SHADER, fragment_shader_code);
+    gl_program_ = glCreateProgram();
+    glAttachShader(gl_program_, vertex_shader_);
+    glAttachShader(gl_program_, fragment_shader_);
+    glLinkProgram(gl_program_);
+    glDeleteShader(vertex_shader_);
+    glDeleteShader(fragment_shader_);
+    mvp_matrix_location_ = glGetUniformLocation(gl_program_, "mvpMatrix");
+    assert(mvp_matrix_location_ >= 0);
+    vertex_location_ = glGetAttribLocation(gl_program_, "position");
+    assert(vertex_location_ >= 0);
+    color_location_ = glGetUniformLocation(gl_program_, "color");
+    assert(color_location_ >= 0);
+  }
+
+  GLuint prepare_shader(GLenum type, const char* shader_code) {
+      GLuint shaderId = glCreateShader(type);
+      GLint length = (GLint)std::strlen(shader_code);
+      glShaderSource(shaderId, 1, &shader_code, &length);
+      glCompileShader(shaderId);
+      GLint result;
+      glGetShaderiv(shaderId, GL_COMPILE_STATUS, &result);
+      if (result == (GLint)GL_FALSE) {
+          GLint logSize = 0;
+          glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &logSize);
+
+          std::vector<GLchar> errorLog(logSize);
+          glGetShaderInfoLog(shaderId, logSize, &logSize, errorLog.data());
+
+          glDeleteShader(shaderId);
+          return 0;
+      }
+      return shaderId;
+
   }
 
   void paintGL() {
-    qglClearColor(QColor::fromRgb(255, 255, 255));
+    glClearColor(1.f, 1.f, 1.f, 1.f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
     draw_points();
     draw_segments();
     draw_vertices();
@@ -127,6 +194,11 @@ class GLWidget : public QGLWidget {
     point_data_.clear();
     segment_data_.clear();
     vd_.clear();
+
+    clear_vbo_array(gl_points_);
+    clear_vbo(gl_segments_);
+    clear_vbo_array(gl_vertices_);
+    clear_vbo_array(gl_edges_);
   }
 
   void read_data(const QString& file_path) {
@@ -193,99 +265,218 @@ class GLWidget : public QGLWidget {
   }
 
   void update_view_port() {
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
     rect_type view_rect = brect_;
     deconvolve(view_rect, shift_);
-    glOrtho(xl(view_rect), xh(view_rect),
-            yl(view_rect), yh(view_rect),
-            -1.0, 1.0);
-    glMatrixMode(GL_MODELVIEW);
+    const float width = xh(view_rect) - xl(view_rect);
+    const float height = yh(view_rect) - yl(view_rect);
+    projection_matrix_[0] = 2.f / width;
+    projection_matrix_[5] = 2.f / height;
+    projection_matrix_[10] = -1.f;
+    projection_matrix_[15] = 1.f;
+    projection_matrix_[12] = -(xl(view_rect) + xh(view_rect)) / width;
+    projection_matrix_[13] = -(yl(view_rect) + yh(view_rect)) / height;
+    projection_matrix_[14] = 0.f;
+  }
+
+  struct GLPoint {
+    GLPoint(float x, float y)
+      : x_(x), y_(y) {}
+    float x_;
+    float y_;
+  };
+  struct VBO {
+    VBO(GLuint id, size_t vertex_count)
+      : id_(id), vertex_count_(vertex_count) {}
+    GLuint id_;
+    size_t vertex_count_;
+  };
+
+  VBO point_vbo(const point_type& point, float radius_px) {
+      const float width = xh(brect_) - xl(brect_);
+      const float height = yh(brect_) - yl(brect_);
+      const float xRadius = radius_px * width / size().width();
+      const float yRadius = radius_px * height / size().height();
+      static constexpr size_t boundary_point_count = 20;
+      static constexpr float angle_increment = 2 * 3.14159265358979323846 / boundary_point_count;
+      std::vector<GLPoint> boundary;
+      boundary.reserve(boundary_point_count + 2);
+      boundary.emplace_back(point.x(), point.y());
+      for (int i = 0; i <= boundary_point_count; ++i) {
+          const float angle = angle_increment * i;
+          boundary.emplace_back(point.x() + std::sin(angle) * xRadius, point.y() + std::cos(angle) * yRadius);
+      }
+      GLuint buffer_id;
+      glGenBuffers(1, &buffer_id);
+      glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
+      glBufferData(GL_ARRAY_BUFFER, boundary.size() * sizeof(GLPoint), boundary.data(), GL_STATIC_DRAW);
+      return VBO(buffer_id, boundary.size());
+  }
+
+  void clear_vbo(VBO& vbo) {
+      glDeleteBuffers(1, &vbo.id_);
+      vbo.id_ = 0;
+  }
+
+  void clear_vbo_array(std::vector<VBO>& vbos) {
+      for (VBO& vbo : vbos) {
+          clear_vbo(vbo);
+      }
+      vbos.clear();
+  }
+
+  void prepare_points() {
+      static constexpr float radius = 4.5f;
+      if (!gl_points_.empty()) {
+          return;
+      }
+      gl_points_.reserve(point_data_.size() + 2 * segment_data_.size());
+      for (std::size_t i = 0; i < point_data_.size(); ++i) {
+          point_type point = point_data_[i];
+          point = deconvolve(point, shift_);
+          gl_points_.push_back(point_vbo(point, radius));
+      }
+      for (std::size_t i = 0; i < segment_data_.size(); ++i) {
+          point_type lp = low(segment_data_[i]);
+          lp = deconvolve(lp, shift_);
+          gl_points_.push_back(point_vbo(lp, radius));
+          point_type hp = high(segment_data_[i]);
+          hp = deconvolve(hp, shift_);
+          gl_points_.push_back(point_vbo(hp, radius));
+      }
   }
 
   void draw_points() {
     // Draw input points and endpoints of the input segments.
-    glColor3f(0.0f, 0.5f, 1.0f);
-    glPointSize(9);
-    glBegin(GL_POINTS);
-    for (std::size_t i = 0; i < point_data_.size(); ++i) {
-      point_type point = point_data_[i];
-      deconvolve(point, shift_);
-      glVertex2f(point.x(), point.y());
+    prepare_points();
+    glUseProgram(gl_program_);
+    glUniformMatrix4fv(mvp_matrix_location_, 1, GL_FALSE, projection_matrix_.data());
+    std::array<float, 4> color{0.0f, 0.5f, 1.0f, 1.0f};
+    glUniform4fv(color_location_, 1, color.data());
+    for (const VBO& point_vbo : gl_points_) {
+        glBindBuffer(GL_ARRAY_BUFFER, point_vbo.id_);
+        glVertexAttribPointer(vertex_location_, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+        glEnableVertexAttribArray(vertex_location_);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, (GLsizei)point_vbo.vertex_count_);
+        glDisableVertexAttribArray(vertex_location_);
     }
-    for (std::size_t i = 0; i < segment_data_.size(); ++i) {
-      point_type lp = low(segment_data_[i]);
-      lp = deconvolve(lp, shift_);
-      glVertex2f(lp.x(), lp.y());
-      point_type hp = high(segment_data_[i]);
-      hp = deconvolve(hp, shift_);
-      glVertex2f(hp.x(), hp.y());
-    }
-    glEnd();
   }
 
+  void prepare_segments() {
+      if (gl_segments_.id_ != 0) {
+          return;
+      }
+      std::vector<GLPoint> segment_points;
+      segment_points.reserve(segment_data_.size() * 2);
+      for (std::size_t i = 0; i < segment_data_.size(); ++i) {
+          point_type lp = low(segment_data_[i]);
+          lp = deconvolve(lp, shift_);
+          segment_points.emplace_back(lp.x(), lp.y());
+          point_type hp = high(segment_data_[i]);
+          hp = deconvolve(hp, shift_);
+          segment_points.emplace_back(hp.x(), hp.y());
+      }
+      glGenBuffers(1, &gl_segments_.id_);
+      glBindBuffer(GL_ARRAY_BUFFER, gl_segments_.id_);
+      glBufferData(GL_ARRAY_BUFFER, segment_points.size() * sizeof(GLPoint), segment_points.data(), GL_STATIC_DRAW);
+      gl_segments_.vertex_count_ = segment_points.size();
+  }
   void draw_segments() {
     // Draw input segments.
-    glColor3f(0.0f, 0.5f, 1.0f);
+    prepare_segments();
+    glUseProgram(gl_program_);
+    glUniformMatrix4fv(mvp_matrix_location_, 1, GL_FALSE, projection_matrix_.data());
+    std::array<float, 4> color{0.0f, 0.5f, 1.0f, 1.0f};
+    glUniform4fv(color_location_, 1, color.data());
+    glBindBuffer(GL_ARRAY_BUFFER, gl_segments_.id_);
+    glVertexAttribPointer(vertex_location_, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
     glLineWidth(2.7f);
-    glBegin(GL_LINES);
-    for (std::size_t i = 0; i < segment_data_.size(); ++i) {
-      point_type lp = low(segment_data_[i]);
-      lp = deconvolve(lp, shift_);
-      glVertex2f(lp.x(), lp.y());
-      point_type hp = high(segment_data_[i]);
-      hp = deconvolve(hp, shift_);
-      glVertex2f(hp.x(), hp.y());
-    }
-    glEnd();
+    glEnableVertexAttribArray(vertex_location_);
+    glDrawArrays(GL_LINES, 0, (GLsizei)gl_segments_.vertex_count_);
+    glDisableVertexAttribArray(vertex_location_);
   }
 
+  void prepare_vertices() {
+      static constexpr float radius = 3;
+      if (!gl_vertices_.empty()) {
+          return;
+      }
+      for (const_vertex_iterator it = vd_.vertices().begin();
+           it != vd_.vertices().end(); ++it) {
+          if (internal_edges_only_ && (it->color() == EXTERNAL_COLOR)) {
+              continue;
+          }
+          point_type vertex(it->x(), it->y());
+          vertex = deconvolve(vertex, shift_);
+          gl_vertices_.push_back(point_vbo(vertex, radius));
+      }
+  }
   void draw_vertices() {
     // Draw voronoi vertices.
-    glColor3f(0.0f, 0.0f, 0.0f);
-    glPointSize(6);
-    glBegin(GL_POINTS);
-    for (const_vertex_iterator it = vd_.vertices().begin();
-         it != vd_.vertices().end(); ++it) {
-      if (internal_edges_only_ && (it->color() == EXTERNAL_COLOR)) {
-        continue;
-      }
-      point_type vertex(it->x(), it->y());
-      vertex = deconvolve(vertex, shift_);
-      glVertex2f(vertex.x(), vertex.y());
+    prepare_vertices();
+    glUseProgram(gl_program_);
+    glUniformMatrix4fv(mvp_matrix_location_, 1, GL_FALSE, projection_matrix_.data());
+    std::array<float, 4> color{0.0f, 0.0f, 0.0f, 1.0f};
+    glUniform4fv(color_location_, 1, color.data());
+    for (const VBO& vertex_vbo : gl_vertices_) {
+      glBindBuffer(GL_ARRAY_BUFFER, vertex_vbo.id_);
+      glVertexAttribPointer(vertex_location_, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+      glEnableVertexAttribArray(vertex_location_);
+      glDrawArrays(GL_TRIANGLE_FAN, 0, (GLsizei)vertex_vbo.vertex_count_);
+      glDisableVertexAttribArray(vertex_location_);
     }
-    glEnd();
+  }
+
+  void prepare_edges() {
+      if (!gl_edges_.empty()) {
+          return;
+      }
+      for (const_edge_iterator it = vd_.edges().begin();
+           it != vd_.edges().end(); ++it) {
+          if (primary_edges_only_ && !it->is_primary()) {
+              continue;
+          }
+          if (internal_edges_only_ && (it->color() == EXTERNAL_COLOR)) {
+              continue;
+          }
+          std::vector<point_type> samples;
+          if (!it->is_finite()) {
+              clip_infinite_edge(*it, &samples);
+          } else {
+              point_type vertex0(it->vertex0()->x(), it->vertex0()->y());
+              samples.push_back(vertex0);
+              point_type vertex1(it->vertex1()->x(), it->vertex1()->y());
+              samples.push_back(vertex1);
+              if (it->is_curved()) {
+                  sample_curved_edge(*it, &samples);
+              }
+          }
+          std::vector<GLPoint> gl_samples;
+          gl_samples.reserve(samples.size());
+          for (point_type& sample : samples) {
+              point_type vertex = deconvolve(sample, shift_);
+              gl_samples.emplace_back(sample.x(), sample.y());
+          }
+          GLuint vbo_id;
+          glGenBuffers(1, &vbo_id);
+          glBindBuffer(GL_ARRAY_BUFFER, vbo_id);
+          glBufferData(GL_ARRAY_BUFFER, gl_samples.size() * sizeof(GLPoint), gl_samples.data(), GL_STATIC_DRAW);
+          gl_edges_.emplace_back(vbo_id, gl_samples.size());
+      }
   }
   void draw_edges() {
     // Draw voronoi edges.
-    glColor3f(0.0f, 0.0f, 0.0f);
+    prepare_edges();
+    glUseProgram(gl_program_);
+    glUniformMatrix4fv(mvp_matrix_location_, 1, GL_FALSE, projection_matrix_.data());
+    std::array<float, 4> color{0.0f, 0.0f, 0.0f, 1.0f};
+    glUniform4fv(color_location_, 1, color.data());
     glLineWidth(1.7f);
-    for (const_edge_iterator it = vd_.edges().begin();
-         it != vd_.edges().end(); ++it) {
-      if (primary_edges_only_ && !it->is_primary()) {
-        continue;
-      }
-      if (internal_edges_only_ && (it->color() == EXTERNAL_COLOR)) {
-        continue;
-      }
-      std::vector<point_type> samples;
-      if (!it->is_finite()) {
-        clip_infinite_edge(*it, &samples);
-      } else {
-        point_type vertex0(it->vertex0()->x(), it->vertex0()->y());
-        samples.push_back(vertex0);
-        point_type vertex1(it->vertex1()->x(), it->vertex1()->y());
-        samples.push_back(vertex1);
-        if (it->is_curved()) {
-          sample_curved_edge(*it, &samples);
-        }
-      }
-      glBegin(GL_LINE_STRIP);
-      for (std::size_t i = 0; i < samples.size(); ++i) {
-        point_type vertex = deconvolve(samples[i], shift_);
-        glVertex2f(vertex.x(), vertex.y());
-      }
-      glEnd();
+    for (const VBO& edge_vbo : gl_edges_) {
+        glBindBuffer(GL_ARRAY_BUFFER, edge_vbo.id_);
+        glVertexAttribPointer(vertex_location_, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+        glEnableVertexAttribArray(vertex_location_);
+        glDrawArrays(GL_LINES, 0, (GLsizei)edge_vbo.vertex_count_);
+        glDisableVertexAttribArray(vertex_location_);
     }
   }
 
@@ -382,6 +573,18 @@ class GLWidget : public QGLWidget {
   bool brect_initialized_;
   bool primary_edges_only_;
   bool internal_edges_only_;
+
+  std::array<float, 16> projection_matrix_{};
+  std::vector<VBO> gl_points_;
+  VBO gl_segments_{0, 0};
+  std::vector<VBO> gl_vertices_;
+  std::vector<VBO> gl_edges_;
+  GLuint gl_program_;
+  GLuint vertex_shader_;
+  GLuint fragment_shader_;
+  GLint mvp_matrix_location_;
+  GLint vertex_location_;
+  GLint color_location_;
 };
 
 class MainWindow : public QWidget {
@@ -433,7 +636,7 @@ class MainWindow : public QWidget {
 
   void print_scr() {
     if (!file_name_.isEmpty()) {
-      QImage screenshot = glWidget_->grabFrameBuffer(true);
+      QImage screenshot = glWidget_->grabFramebuffer();
       QString output_file = file_dir_.absolutePath() + tr("/") +
           file_name_.left(file_name_.indexOf('.')) + tr(".png");
       screenshot.save(output_file, 0, -1);
@@ -500,6 +703,11 @@ class MainWindow : public QWidget {
 };
 
 int main(int argc, char* argv[]) {
+  QSurfaceFormat surfaceFormat = QSurfaceFormat::defaultFormat();
+  surfaceFormat.setSamples(4);
+  surfaceFormat.setProfile(QSurfaceFormat::OpenGLContextProfile::CompatibilityProfile);
+  QSurfaceFormat::setDefaultFormat(surfaceFormat);
+
   QApplication app(argc, argv);
   MainWindow window;
   window.show();
